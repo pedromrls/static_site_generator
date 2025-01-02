8e0131634654c44f2dc63660efe6c4698b146c04
Pedromrls <pedromrls6@gmail.com>
2025-01-02 11:51:57 -0500

    added the leafnode and test cases

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
+from leafnode import LeafNode
 
+prop_example2={
+    "href": "https://www.google.com",
+}
 
 def main():
     cato = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    gato = TextNode("This is a text node", TextType.BOLD, None)
+    node = HTMLNode("a", "This is a string representing the value of the HTML tag",[],prop_example2)
+    lfnode = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
     print(repr(cato))
-    print(repr(gato))
+    print(repr(node))
+    print(lfnode.to_html())
+
+
 
 
 if __name__ == "__main__":
---
+++ src/test_htmlnode.py
@@ -0,0 +1,46 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+
+    prop_example = {
+    "href": "https://www.google.com", 
+    "target": "_blank",
+}
+    prop_example2={
+    "href": "https://www.google.com",
+}
+
+
+    def test_to_html_not_implemented(self):
+        with self.assertRaises(NotImplementedError):
+            node = HTMLNode()
+            node.to_html()
+
+    def test_props_to_html(self):
+        node = HTMLNode(
+            props={
+                "href": "https://www.google.com",
+                "target": "_blank",
+            }
+        )
+        self.assertEqual(' href="https://www.google.com" target="_blank"', node.props_to_html())
+    
+    def test_props_to_html_none(self):
+        node = HTMLNode()
+        self.assertEqual('', node.props_to_html())
+    
+    def test_props_to_html_emty_dict(self):
+        node = HTMLNode(props={})
+        self.assertEqual('', node.props_to_html())
+
+    def test_repr(self):
+        node = HTMLNode("a", "This is a string representing the value of the HTML tag",[],self.prop_example2)
+        self.assertEqual("HTMLNode(a, This is a string representing the value of the HTML tag,[], {'href': 'https://www.google.com'})", repr(node))
+       
+
+
+if __name__ == "__main__":
+    unittest.main()
