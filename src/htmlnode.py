9e6b593a03630f0401945f0332146260f8c98466
Pedromrls <pedromrls6@gmail.com>
2025-01-02 12:03:34 -0500

    bundled htmlnode child classes in same file and deleted redundant files, fixed module import

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,7 +4,7 @@ class HTMLNode:
         self.value = value
         self.children = children
         self.props = props
-    
+
     def to_html(self):
         raise NotImplementedError
 
@@ -12,7 +12,26 @@ class HTMLNode:
         if not self.props:
             return ""
         return " " + " ".join(f'{key}="{value}"' for key, value in self.props.items())
-                              
+
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value},{self.children}, {self.props})" 
-    
+        return f"HTMLNode({self.tag}, {self.value},{self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Invalid HTML: no value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -1,45 +1,52 @@
 import unittest
 
-from leafnode import LeafNode
+from htmlnode import LeafNode
+
 
 class TestLeafNode(unittest.TestCase):
 
-    prop_example={
-    "href": "https://www.google.com",
-}
+    prop_example = {
+        "href": "https://www.google.com",
+    }
     prop_example2 = {
-    "href": "https://www.google.com", 
-    "target": "_blank",
-}
+        "href": "https://www.google.com",
+        "target": "_blank",
+    }
     paragraph = "This is a paragraph of text."
 
     text = "Click me!"
 
     def test_empty_value_raises_error(self):
         with self.assertRaises(ValueError):
-            node = LeafNode(None,"")
+            node = LeafNode(None, "")
             node.to_html()
 
     def test_empty_value_raises_error2(self):
         with self.assertRaises(ValueError):
-            node = LeafNode(None,None)
+            node = LeafNode(None, None)
             node.to_html()
 
     def test_to_html_none_tag(self):
         node = LeafNode(None, self.paragraph)
         self.assertEqual("This is a paragraph of text.", node.to_html())
-    
+
     def test_to_html_single_prop(self):
         node = LeafNode("a", self.text, self.prop_example)
-        self.assertEqual('<a href="https://www.google.com">Click me!</a>', node.to_html())
+        self.assertEqual(
+            '<a href="https://www.google.com">Click me!</a>', node.to_html()
+        )
 
     def test_to_html_multiple_props(self):
         node = LeafNode("a", self.text, self.prop_example2)
-        self.assertEqual('<a href="https://www.google.com" target="_blank">Click me!</a>', node.to_html())
-    
+        self.assertEqual(
+            '<a href="https://www.google.com" target="_blank">Click me!</a>',
+            node.to_html(),
+        )
+
     def test_to_html_prop_no_tag(self):
         node = LeafNode(None, self.text)
-        self.assertEqual('Click me!', node.to_html())
+        self.assertEqual("Click me!", node.to_html())
+
 
 if __name__ == "__main__":
     unittest.main()
