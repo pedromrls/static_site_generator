df0456be96e3c3206e4aafffe557d5e35781241d
Pedromrls <pedromrls6@gmail.com>
2025-01-06 13:11:25 -0500

    no much to say

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -32,3 +32,11 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def split_nodes_image(old_nodes):
+    pass
+
+
+def split_nodes_link(old_nodes):
+    pass
