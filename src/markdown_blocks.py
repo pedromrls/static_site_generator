da394debed5ca5fdda828a4d7b73fc5cd956cd43
Pedromrls <pedromrls6@gmail.com>
2025-01-12 11:27:43 -0500

    insert generic comment about the code

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,21 @@
 import re
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from inline_markdown import *
+
+block_heading = "heading"
+block_code = "code"
+block_quote = "quote"
+block_ul = "unordered_list"
+block_ol = "ordered_list"
+block_paragraph = "paragraph"
+heading_dict = {
+    "#": "h1",
+    "##": "h2",
+    "###": "h3",
+    "####": "h4",
+    "#####": "h5",
+    "######": "h6",
+}
 
 
 def markdown_to_blocks(markdown):
@@ -7,26 +24,58 @@ def markdown_to_blocks(markdown):
 
 def block_to_block_type(block):
     if re.match(r"^#{1,6} ", block):
-        return "heading"
+        return block_heading
     elif re.match(r"^```(.*?)```$", block):
-        return "code"
+        return block_code
     elif block[0] == ">":
-        return "quote"
+        return block_quote
     elif re.match(r"^[\*\-] ", block):
         lines = block.split("\n")
         for line in lines:
             if not re.match(r"^[\*\-] ", line):
-                return "paragraph"
-        return "unordered_list"
+                return block_paragraph
+        return block_ul
     elif re.match(r"^\d+\. ", block):
         # subject to change
 
         lines = block.split("\n")
         if lines[0][0] != "1":
-            return "paragraph"
+            return block_paragraph
         for i, v in enumerate(lines):
             if v[:3] != f"{i + 1}. ":
-                return "paragraph"
-        return "ordered_list"
+                return block_paragraph
+        return block_ol
     else:
-        return "paragraph"
+        return block_paragraph
+
+
+def markdown_to_html_node(markdown):
+    # 1. split the markdown
+    blocks = markdown_to_blocks(markdown)
+    # 2. Process for each block
+    for block in blocks:
+        # 2.1. identify the kind of block
+
+        block_type = block_to_block_type(block)
+        # 2.2. Create a corresponding HTMLNode for that block type
+        if block_type == block_heading:
+
+            count = block.count("#")
+            heading_tag = heading_dict("#" * count)
+            heading_content = block[count:].strip()
+            parent = ParentNode(heading_tag, text_to_textnodes(heading_content))
+
+        elif block_type == block_code:
+            pass
+        elif block_type == block_quote:
+            pass
+        elif block_type == block_ol:
+            pass
+        elif block_type == block_ul:
+            pass
+        elif block_type == block_paragraph:
+            pass
+
+
+def text_to_children(text):
+    pass
