88415061a920a53b3caac6b84538724de18ae23d
Pedromrls <pedromrls6@gmail.com>
2025-01-18 13:30:42 -0500

    Working on the initial logic for cleaning and creating the public directory

--- src/main.py
+++ src/main.py
@@ -1,22 +1,36 @@
 from textnode import TextNode, TextType, text_node_to_html_node
 from htmlnode import HTMLNode, LeafNode
+import os
+import shutil
 
-prop_example2 = {
-    "href": "https://www.google.com",
-}
+static_dir = "static"
+public_dir = "public"
+
+def list_files(static_dir):
+    file_list = []
+    if os.path.isfile(static_dir):
+        file_list.append(static_dir)
+    else: 
+        for link in os.listdir(static_dir):
+            new_filepath = os.path.join(static_dir, link)
+            if os.path.isfile(link):
+                file_list.append(new_filepath)
+            else:
+                file_list.extend(list_files(new_filepath))
+    return file_list
+
+def cleaner():
+    
+    if not os.path.exists(static_dir):
+        raise Exception(f"No {static_dir}directory found")
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir)
+    file_list = list_files(static_dir)
+    print(file_list)
 
 
 def main():
-    cato = TextNode("This is a text node", TextType.IMAGE, "https://www.boot.dev")
-    node = HTMLNode(
-        "a",
-        "This is a string representing the value of the HTML tag",
-        [],
-        prop_example2,
-    )
-    lfnode = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-    print(text_node_to_html_node(cato).to_html())
-
-
+    cleaner()
+    pass
 if __name__ == "__main__":
     main()
